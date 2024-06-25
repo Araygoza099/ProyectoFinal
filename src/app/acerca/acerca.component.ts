a6ce7642f4076e8e5049830769bc31b74768f7a6
ZurielZD <160747576+ZurysZD@users.noreply.github.com>
2024-06-24 19:08:03 -0600

    Formulario de correo en Acerca

--- src/app/acerca/acerca.component.ts
+++ src/app/acerca/acerca.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-acerca',
   standalone: true,
-  imports: [],
   templateUrl: './acerca.component.html',
-  styleUrl: './acerca.component.css'
+  styleUrl: './acerca.component.css',
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class AcercaComponent {
+  contactoForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactoForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit(){
+    if(this.contactoForm.valid){
+      console.log('Formulario válido', this.contactoForm.value);
+    }else{
+      console.log('Formulario inválido');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AcercaComponent } from './acerca/acerca.component';
 
 @NgModule({
-  declarations: [AnimalesComponent],
-  imports: [NgxMasonryModule, BrowserModule, AppRoutingModule, MatSnackBarModule],
-
+  declarations: [AnimalesComponent, AcercaComponent],
+  imports: [NgxMasonryModule, BrowserModule, AppRoutingModule, MatSnackBarModule, ReactiveFormsModule],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
