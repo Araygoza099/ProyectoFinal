176dde18631d6dd51803c80873640bbe4bda91a0
ZurielZD <160747576+ZurysZD@users.noreply.github.com>
2024-06-25 15:12:34 -0600

    Revert "Merge branch 'main' into Front-end"
    This reverts commit 03d82b30e77afa399867d3d6552f461b5c71b716, reversing
    changes made to d75bd143703ac09898f7edab18e61c32d0e8ddf8.

--- src/app/acerca/acerca.component.ts
+++ src/app/acerca/acerca.component.ts
@@ -1,45 +1,15 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import * as QRCode from 'qrcode';
 import { ReactiveFormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-acerca',
   standalone: true,
-  imports: [CommonModule,FormsModule],
   templateUrl: './acerca.component.html',
   styleUrl: './acerca.component.css',
   imports: [CommonModule, ReactiveFormsModule]
 })
 export class AcercaComponent {
-  qrData: string = '';
-  qrCodeUrl: string = '';
-
-  constructor(private http: HttpClient) {}
-
-  generateQRCode() {
-    this.http.get<{ qrData: any }>('http://localhost:3000/get-data')
-      .subscribe({
-        next: (response) => {
-          this.qrData = JSON.stringify(response.qrData, null, 2);
-          QRCode.toDataURL(this.qrData, (err, url) => {
-            if (err) {
-              console.error('Error generating QR code', err);
-            } else {
-              this.qrCodeUrl = url;
-            }
-          });
-        },
-        error: (error) => {
-          console.error('Error al obtener los datos', error);
-        }
-      });
-  }
-
-}
-
   contactoForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
