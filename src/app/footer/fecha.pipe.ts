bdd5ed42600515a8a59b1a8c66505dda47073d1c
Sergio Valdivia <Valdivia_sergio@outlook.com>
2024-06-26 13:06:53 -0600

    Cambio Pipe

--- src/app/footer/fecha.pipe.ts
+++ src/app/footer/fecha.pipe.ts
@@ -12,8 +12,11 @@ export class FechaPipe implements PipeTransform {
     const day = value.getDate().toString().padStart(2, '0');
     const month = (value.getMonth() + 1).toString().padStart(2, '0');
     const year = value.getFullYear();
+    const hours = value.getHours().toString().padStart(2, '0');
+    const minutes = value.getMinutes().toString().padStart(2, '0');
+    const seconds = value.getSeconds().toString().padStart(2, '0');
 
-    return `${day}/${month}/${year}`;
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
   }
 
 }
